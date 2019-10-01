4cdc864acc2f5d4465fcca4ba8aeb7385337cf64
Danial Ali <thedanial.ali@gmail.com>
2019-10-01 00:28:50 -0400

    Added the custom day view - now able to see everyone scheduled for today Added the custom calendar to accommodate the red dot Fixed common bugs that caused the app to crash upon initial launch Added patch to prevent user who isn't part of a schedule from viewing schedule

---
+++ app/src/main/java/com/theogapplepie/mywalmartschedule/Employee.java
@@ -0,0 +1,15 @@
+package com.theogapplepie.mywalmartschedule;
+
+public class Employee {
+    String badgeNumber;
+    public Employee(){
+
+    }
+    public Employee(String badgeNumber){
+        this.badgeNumber = badgeNumber;
+    }
+
+    public String getBadgeNumber() {
+        return badgeNumber;
+    }
+}
